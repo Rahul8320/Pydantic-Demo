34d2c64c80cc926d3e8b537b8bc185bbb9c3d5cd
Rahul Dey <rahuldey832000@gmail.com>
2025-05-24 16:22:24 +0530

    Added user model

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+from typing import Dict
 from models.employee import Employee
+from models.user import SignupDto, User
 
 
 def main():
     print("Hello from pydantic!")
 
-    employee: dict[str, str | int] = {
+    employee: Dict[str, str | int] = {
         "id": 12,
         "name": "Rahul Dey",
         "salary": 45000,
     }
 
     print(Employee(**employee))  # type: ignore
+
+    user: Dict[str, str] = {"username": "test"}
+    print(User(**user))
+
+    signupDto: Dict[str, str] = {
+        "password": "password",
+        "confirm_password": "password",
+    }
+    print(SignupDto(**signupDto))
 
 
 if __name__ == "__main__":
---
+++ models/user.py
@@ -0,0 +1,25 @@
+from typing import Any
+from pydantic import BaseModel, Field, field_validator, model_validator
+
+
+class User(BaseModel):
+    username: str
+
+    @field_validator("username")
+    def username_length(cls, v: str):
+        if len(v) < 4:
+            raise ValueError("Username must be at least 4 characters long")
+
+        return v
+
+
+class SignupDto(BaseModel):
+    password: str = Field(..., min_length=6, max_length=15)
+    confirm_password: str = Field(..., min_length=6, max_length=15)
+
+    @model_validator(mode="after")
+    def password_match(cls, values: Any):
+        if values.password != values.confirm_password:
+            raise ValueError("Password do not match")
+
+        return values
